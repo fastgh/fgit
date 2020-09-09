9cb672715b6c039fe7189a973f886057b4c53257
fastgh <fastgithub@gmail.com>
2020-09-09 14:54:41 +0800

    a little bit more refactor

--- build.sh
+++ build.sh
@@ -5,20 +5,10 @@ set -e
 
 PROJECT_DIR=$(cd "$(dirname $0)";pwd)
 
-INSTRUMENT_SRC=git_instrument.go
-INSTRUMENT_SRC_PRIVATE=${PROJECT_DIR}/../${INSTRUMENT_SRC}
-INSTRUMENT_SRC_PUB=${PROJECT_DIR}/${INSTRUMENT_SRC}
-INSTRUMENT_SRC_PUB_BACKUP=${INSTRUMENT_SRC_PUB}.backup
-
 BUILD_DIR=${PROJECT_DIR}/build
 
 rm -rf ${BUILD_DIR}
 cd ${PROJECT_DIR}
-
-mv ${INSTRUMENT_SRC_PUB} ${INSTRUMENT_SRC_PUB_BACKUP}
-if [ -f ${INSTRUMENT_SRC_PRIVATE} ];then
-  cp ${INSTRUMENT_SRC_PRIVATE} ${INSTRUMENT_SRC_PUB}
-fi
 
 go_build() {
     local _OS=$1
@@ -40,5 +30,3 @@ go_build linux
 go_build darwin
 go_build windows
 
-rm ${INSTRUMENT_SRC_PUB}
-mv ${INSTRUMENT_SRC_PUB_BACKUP} ${INSTRUMENT_SRC_PUB}
--- git_instrument.go
+++ git_instrument.go
@@ -1,10 +1,5 @@
 package main
 
-// SetGitRemoteURL ...
-func SetGitRemoteURL(workDir string, remoteName string, remoteURL string) {
-	ExecGit(workDir, []string{"remote", "set-url", "origin", remoteURL})
-}
-
 // GithubInstrument ...GithubInstrument
 func GithubInstrument(isPrivate bool, config Config) {
 	var global bool
@@ -17,22 +12,8 @@ func GithubInstrument(isPrivate bool, config Config) {
 		workDir = ""
 	}
 
-	if isPrivate {
-		//TODO: recover
-		oldHTTPProxy, oldHTTPSProxy := ConfigGitHTTPProxy(workDir, global, config.Proxy, config.Proxy)
-		oldGit(true, false)
-		ResetGitHTTPProxy(workDir, global, oldHTTPProxy, oldHTTPSProxy)
-	} else {
-		//TODO:  drecover
-		newCloneURL := InstrumentURLwithMirror(cmdline.GitURLText, config.Mirror)
-
-		if !cmdline.IsGitClone {
-			SetGitRemoteURL(workDir, cmdline.GitRemoteName, newCloneURL)
-		} else {
-			cmdline.Args[cmdline.ArgIndexOfGitURLText-1] = newCloneURL
-		}
-
-		oldGit(true, false)
-		SetGitRemoteURL(workDir, cmdline.GitRemoteName, cmdline.GitURLText)
-	}
+	//TODO: recover
+	oldHTTPProxy, oldHTTPSProxy := ConfigGitHTTPProxy(workDir, global, config.Proxy, config.Proxy)
+	oldGit(true, false)
+	ResetGitHTTPProxy(workDir, global, oldHTTPProxy, oldHTTPSProxy)
 }
